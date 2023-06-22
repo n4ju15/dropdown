998b9d5a3ed2edfc551d1fd0d65ed4d0afb1f906
n4ju15 <anajulia.garialbano15@gmail.com>
2023-06-22 18:31:17 -0300

    Scripts finais

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
+const backToTopBtn = document.querySelector("#back-to-top");
+
 window.addEventListener("scroll", function() {
 
-    const scrollTop = window.pageYOffset
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-})
+    if(scrollTop > 500) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+});
+
+backToTopBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
